724adb6898f39dce9f4d8a4b217a614e2d9ba1fd
argrubbs <argrubbs@users.noreply.github.com>
2023-02-26 12:37:20 -0600

    feat: added alsa support and tailscale

--- sound.nix
+++ sound.nix
@@ -5,6 +5,10 @@
   services.pipewire = {
     enable = true;
     audio.enable = true;
+    alsa = {
+      enable = true;
+      support32Bit = true;
+    };
     pulse.enable = true;
     socketActivation = true;
     wireplumber.enable = true;
